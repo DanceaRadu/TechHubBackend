e6579f90d4a6d76b659a7d87835aff2a9d6efe1e
DanceaRadu <radu.dancea@gmail.com>
2023-06-28 18:59:07 +0300

    Project creation.

---
+++ src/main/java/com/tech/techhubbackend/TechHubBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tech.techhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechHubBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tech/techhubbackend/TechHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.techhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
