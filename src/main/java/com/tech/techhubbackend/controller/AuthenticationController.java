c8511738648ad41e9e33fad732314245811c10fb
DanceaRadu <radu.dancea@gmail.com>
2023-06-29 19:55:06 +0300

    Added cross-origin for frontend.

--- src/main/java/com/tech/techhubbackend/controller/AuthenticationController.java
+++ src/main/java/com/tech/techhubbackend/controller/AuthenticationController.java
@@ -6,12 +6,10 @@ import com.tech.techhubbackend.DTO.DTOs.RegisterRequest;
 import com.tech.techhubbackend.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
