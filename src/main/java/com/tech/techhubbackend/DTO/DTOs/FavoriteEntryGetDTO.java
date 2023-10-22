9577955368fa76f45efeead1ab4f981336c5d13d
DanceaRadu <radu.dancea@gmail.com>
2023-10-23 00:39:10 +0300

    Added the product to the FavoriteEntryGetDTO.

--- src/main/java/com/tech/techhubbackend/DTO/DTOs/FavoriteEntryGetDTO.java
+++ src/main/java/com/tech/techhubbackend/DTO/DTOs/FavoriteEntryGetDTO.java
@@ -1,6 +1,7 @@
 package com.tech.techhubbackend.DTO.DTOs;
 
 import com.tech.techhubbackend.model.FavoriteEntry;
+import com.tech.techhubbackend.model.Product;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,10 +13,12 @@ public @Data class FavoriteEntryGetDTO {
     private UUID favoriteID;
     private UUID productID;
     private UUID userID;
+    private Product product;
 
     public FavoriteEntryGetDTO(FavoriteEntry entry) {
         this.favoriteID = entry.getFavoriteID();
         this.productID = entry.getProduct().getProductID();
         this.userID = entry.getUser().getUserID();
+        this.product = entry.getProduct();
     }
 }
